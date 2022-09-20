81e1d177d8e802e94380169dcf05af26042f2302
Mantelmann <Mantelmann@users.noreply.github.com>
2022-09-20 22:53:12 +0200

    Version 0.1.0
    New version with optional name, alpha, better info and error handling!

--- download_images.py
+++ download_images.py
@@ -2,9 +2,9 @@ bl_info = {
     "name": "Image Downloader",
     "description": "Downloads Images from Links and Packs them into your .blend",
     "author": "Mantelmann",
-    "version": (0, 0, 1),
-    "blender": (2, 91, 0),
-    "location": "3D View > Tool",
+    "version": (0, 1, 0),
+    "blender": (3, 3, 0),
+    "location": "3D View > Create",
     "category": "Import-Export",
     "internet": "https://github.com/Mantelmann"
 }
@@ -12,16 +12,14 @@ bl_info = {
 
 import bpy
 
-from bpy.props import (StringProperty,
-                       IntProperty,
-                       FloatProperty,
-                       PointerProperty,
-                       )
+from bpy.props import StringProperty, BoolProperty, PointerProperty
+
 from bpy.types import (Panel,
                        Menu,
                        Operator,
                        PropertyGroup,
                        )
+import os
 import requests
 import shutil
 import tempfile
@@ -29,45 +27,35 @@ import tempfile
 #PRAISE BE
 #https://towardsdatascience.com/how-to-download-an-image-using-python-38a75cfa21c
 
-def download_and_pack(url):
+def download_and_pack(url, custom_name=None):
 
     file_name = url.split("?")[0].split("/")[-1]
 
     try:
-        r = requests.get(url, stream = True)
+        r = requests.get(url.strip(), stream = True)
     except:
-        print(f"Image Downloader: Unable to open URL")
-        return None, None
+        return None, None, f"Image Downloader: Unable to open URL '{url}'"
         
-    if r.status_code == 200:
-        r.raw.decode_content = True
-        
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            
-            #Very disgusting, probably stupid.
-            slash_type = "/"
-            if tmpdirname[0] != "/":
-                slash_type = "\\"
-                
-            file_path = tmpdirname + slash_type + file_name
-            
-            
-            with open(file_path, "wb") as f:
-                shutil.copyfileobj(r.raw, f)
-            
-            img = bpy.data.images.load(file_path)
-            img.pack()
-        
-        print(f"Image Downloader: Successfully Downloaded {file_name}")
-        
-        
-        return img, file_name
+    if r.status_code != 200:
+        return None, None, f"Image Downloader: Unable to Download {file_name}: requests gives Error code {r.status_code}"
 
-    else:
+    if not r.headers["Content-Type"].startswith("image"):
+        return None, None, f"Image Downloader: URL with Mimetype '{r.headers['Content-Type']}' doesn't appear to be an image"
+
+
+    r.raw.decode_content = True
+    
+    tmpfile = tempfile.NamedTemporaryFile(prefix="blender_image_downloader_", suffix="_" + file_name)
+    
+    with open(tmpfile.name, "wb") as f:
+        shutil.copyfileobj(r.raw, f)
         
-        print(f"Image Downloader: Unable to Download {file_name}: requests gives Error code {r.status_code}")
-        
-        return None, None
+    img = bpy.data.images.load(tmpfile.name)
+    img.name = custom_name or file_name
+    img.pack()
+    
+    return img, img.name, f"Image Downloader: Successfully Downloaded {file_name}"
+
     
     
 def create_image_plane(context, image):
@@ -88,7 +76,7 @@ def create_image_plane(context, image):
     
     return obj
 
-def create_image_material(obj, image, name):
+def create_image_material(obj, image, name, use_alpha):
     #PRAISE BE
     #https://blender.stackexchange.com/questions/118646/add-a-texture-to-an-object-using-python-and-blender-2-8
     
@@ -98,32 +86,45 @@ def create_image_material(obj, image, name):
     bsdf = material.node_tree.nodes["Principled BSDF"]
     texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
     texImage.image = image
-    texImage.location = (-250,250)
+    texImage.location = (-350,250)
     material.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
+    
+    if use_alpha:
+        material.node_tree.links.new(bsdf.inputs['Alpha'], texImage.outputs['Alpha'])
+        material.blend_method = "BLEND"
+        material.shadow_method = "CLIP"
     
     obj.data.materials.append(material)
 
 
-def main(context, url):
+def main(context, url, use_alpha, custom_name=None):
     
-    image, name = download_and_pack(url)
+    image, name, message = download_and_pack(url, custom_name=custom_name)
     
     if image == None or name == None:
-        return {'ERROR'}
+        return {'ERROR'}, message
 
     obj = create_image_plane(context, image)
     obj.name = name
 
-    create_image_material(obj, image, name)
+    create_image_material(obj, image, name, use_alpha)
     
-    return {'FINISHED'}
+    return {'INFO'}, message
     
 
 class nbProperties(PropertyGroup):
     
-    image_link: bpy.props.StringProperty(
+    image_link: StringProperty(
         name = "Image Link",
         description = "Direct Link to Image"
+    )
+    custom_name: StringProperty(
+        name = "Custom Name",
+        description = "Custom Name for image instead of URL name. Optional."
+    )
+    use_alpha: BoolProperty(
+        name = "Use Alpha",
+        description = "Sets whether the Alpha Output of the Image shall be used in the generated Material."
     )
 
 class WM_OT_download_image(Operator):
@@ -131,17 +132,23 @@ class WM_OT_download_image(Operator):
     bl_description = "Download Image from the given Link"
     bl_info = "Download"
     bl_idname = "wm.download_image"
+    bl_options = {'UNDO'}
 
     def execute(self, context):
         scene = context.scene
         imdo = scene.image_download
+        
+        if imdo.image_link == "":
+            return {'FINISHED'}
 
-        # print the values to the console
-        if imdo.image_link != "":
-            returned = main(context, imdo.image_link)
-
-        if returned == {'FINISHED'}:
+        returned, message = main(context, imdo.image_link, imdo.use_alpha, custom_name=imdo.custom_name if imdo.custom_name!="" else None)
+            
+        self.report(returned, message)
+        
+        # No Errors, so clear Link and Name
+        if returned == {'INFO'}:
             imdo.image_link = ""
+            imdo.custom_name = ""
 
         return {'FINISHED'}
 
@@ -151,7 +158,7 @@ class OBJECT_PT_CustomPanel(Panel):
     bl_idname = "OBJECT_PT_image_download_panel"
     bl_space_type = "VIEW_3D"   
     bl_region_type = "UI"
-    bl_category = "Tool"
+    bl_category = "Create"
     bl_context = "objectmode"
 
     @classmethod
@@ -164,10 +171,10 @@ class OBJECT_PT_CustomPanel(Panel):
         imdo = scene.image_download
         layout.separator()
         layout.prop(imdo, "image_link")
+        layout.prop(imdo, "custom_name")
+        layout.prop(imdo, "use_alpha")
+        layout.separator()
         layout.operator("wm.download_image")
-
-
-#https://theglorioblog.files.wordpress.com/2019/10/ascendanceofabookworm2.jpg?w=700&h=394
 
 
 classes = (
@@ -187,7 +194,7 @@ def unregister():
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
-    del bpy.types.Scene.imdo
+    del bpy.types.Scene.image_download
 
 
 if __name__ == "__main__":
